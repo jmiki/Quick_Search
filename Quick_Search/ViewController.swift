9cd900b604640b4ff57f0a4adcf011f8f6ef8faa
三木淳司 <junjimiki@junjimikimacbook-puro.local>
2018-10-29 18:23:32 +0900

    Initial Commit

---
+++ Quick_Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quick_Search
+//
+//  Created by 三木淳司 on 2018/10/29.
+//  Copyright © 2018年 Junji Miki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
